f5b2cb634ae4e933d252add8a447349b44978e21
Purit Jessadakannasoon <101883238+Kingsize072@users.noreply.github.com>
2023-10-10 23:01:42 +0000

    how to fix code to draw a texts

---
+++ Drawing.py
@@ -0,0 +1,9 @@
+#number = int(input("Please Enter your number: "))
+#for i in range(number):
+#    text = ""
+#    for j in range(i+1):
+#        text += "*"
+    #print(text)#
+number = int(input("Please Enter your number: "))
+for i in range(number):
+    print("*"*(i+1))
