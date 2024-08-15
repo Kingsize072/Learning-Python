b10f46d937022d8f59c397a27391069eb22c7d08
Purit Jessadakannasoon <101883238+Kingsize072@users.noreply.github.com>
2024-08-15 18:13:15 +0000

    chore: Add age verification functionality to EP13.py

---
+++ EP13.py
@@ -0,0 +1,16 @@
+
+
+#input
+age = int(input("Enter your age: "))
+name = input("Enter your name: ")
+
+# process
+# true
+if age >= 20 :
+    print("You are old enough to drive")
+# false
+else:
+    print("You are not old enough to drive")
+
+#End of program
+print("Thank you for using this program,", name)
